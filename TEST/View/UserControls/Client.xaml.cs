7b946899701ba7be2d9da57fff16b21de297d0cd
Alon123Git <124792243+Alon123Git@users.noreply.github.com>
2023-05-05 11:12:54 +0300

    WpfProjectCommit

---
+++ TEST/View/UserControls/Client.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TEST.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Client.xaml
+    /// </summary>
+    public partial class Client : UserControl
+    {
+        public Client()
+        {
+            InitializeComponent();
+
+            clientTitle.Text = "Youe Credentials";
+            //lblId.Content = "Your ID: ";
+            //lblPas.Content = "Your Password";
+            //btnAccept.Content = "Accept All";
+            //btnManage.Content = "Management Only";
+        }
+    }
+}
---
+++ TEST/View/UserControls/Server.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TEST.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Server.xaml
+    /// </summary>
+    public partial class Server : UserControl
+    {
+        public Server()
+        {
+            InitializeComponent();
+
+            serverTitle.Text = "Login to the server";
+            txbUserName.Text = "User Name";
+            //btnLogin.Content = "Login";
+            //txtBnoAccount.Text = "Don't have an account?";
+            //signUpLink.Text = "Sign Up";
+        }
+    }
+}
